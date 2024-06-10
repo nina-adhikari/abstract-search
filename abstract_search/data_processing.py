364e188bfb28c6c9a20216d4d4a36ee9e3c03b60
Nina A <nachiketa.adhikari@gmail.com>
2024-06-10 16:03:12 +0000

    data preprocessing fix

--- abstract_search/data_processing.py
+++ abstract_search/data_processing.py
@@ -22,29 +22,51 @@ def main():
     df.reset_index(inplace=True)
     df.drop(columns=['index'], inplace=True)
     length = int(len(data) / 10)
+    print(type(df))
+    print(df.head())
 
     for i in range(10):
         offset = i * length
         last = length if i < 9 else len(data) - offset
         print(f'offset: {offset}, last: {offset+last}')
-        batch = df.iloc[offset:offset+last].copy().reset_index()
-        batch.to_feather(f'arxiv_{i}.feather')
+        batch = pd.DataFrame(df.iloc[offset:offset+last].copy())
+        #print(batch.head())
+        batch['text'] = batch['text'].apply(parse_text)
+        #print(batch.head())
+        normalized = pd.json_normalize(batch['text'])
+        normalized.index = batch.index
+        #print(normalized.head())
+        batch = batch.join(normalized, how='left', validate='1:1')
+        #print(batch.head())
+        batch.to_feather(DIR + f'arxiv_{i}_cols.feather')
+        del batch
     
-    df = []
+    del df
+
+    #df = [0]*10
     
+    #for i in range(10):
+    #    df[i]['text'] = df[i]['text'].apply(parse_text)
+    #    df[i] = 0
+
+    # df['text'] = df['text'].apply(parse_text)
+    # df = df.join(pd.json_normalize(df['text']), how='left', validate='1:1').drop(columns=['text'])
+
+    df = [0]*10
     for i in range(10):
-        df[i]['text'] = df[i]['text'].apply(parse_text)
-        df[i] = df[i].join(pd.json_normalize(df[i]['text']), how='left', validate='1:1').drop(columns=['text'])
-        df[i].to_feather(DIR + f'arxiv_{i}_cols.feather')
-        df[i] = 0
-
-    df['text'] = df['text'].apply(parse_text)
-    df = df.join(pd.json_normalize(df['text']), how='left', validate='1:1').drop(columns=['text'])
-
+        df[i] = pd.read_feather(DIR + f'arxiv_{i}_cols.feather')
+    
+    print("Loaded partials")
+    df = pd.concat(df)
+    print(len(df))
+    print(df.head())
     df.to_parquet(DIR + 'arxiv_all.parquet')
+    df['index'] = df.index
     df = df[['index', 'id']]
     df.to_parquet(DIR + 'arxiv_all_small.parquet')
 
+    df = pd.read_parquet(DIR + 'arxiv_all_small.parquet')
+    print(df.head())
 
 if __name__=="__main__":
     main()
