c1756a9531ceb53570baadd48e5a75c1ed119010
Nina A <55521586+nina-adhikari@users.noreply.github.com>
2024-06-10 13:32:51 +0000

    Packaging stuff

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from setuptools import setup
+
+setup(name='abstract_search',
+      version='1.0',
+      description='',
+      url='https://github.com/nina-adhikari/abstract_search',
+      author='Nina Adhikari',
+      author_email='ninamadhikari@gmail.com',
+      # license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
