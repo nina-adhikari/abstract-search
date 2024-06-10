78b95f781d2c37b839ef0466875c1f5a75d2070d
Nina A <55521586+nina-adhikari@users.noreply.github.com>
2024-06-10 16:30:54 +0000

    Refactoring

--- abstract_search/data_processing.py
+++ abstract_search/data_processing.py
@@ -11,8 +11,8 @@ JSON_FILE = DIR + 'arxiv-metadata-oai-snapshot.json'
 def parse_text(text):
     return json.loads(text.split('\n')[0])
 
-def main():
-    with open(JSON_FILE) as json_file:
+def load_json(filename=JSON_FILE):
+    with open(filename) as json_file:
         data = json_file.readlines()
         #data = list(map(json.loads, data))
 
@@ -21,15 +21,15 @@ def main():
     df.rename(columns={0: 'text'}, inplace=True)
     df.reset_index(inplace=True)
     df.drop(columns=['index'], inplace=True)
-    length = int(len(data) / 10)
-    print(type(df))
-    print(df.head())
+    return df
 
-    for i in range(10):
+def write_batches(dataframe, num_batches, dir=DIR):
+    length = int(len(dataframe) / num_batches)
+    for i in range(num_batches):
         offset = i * length
-        last = length if i < 9 else len(data) - offset
+        last = length if i < num_batches-1 else len(dataframe) - offset
         print(f'offset: {offset}, last: {offset+last}')
-        batch = pd.DataFrame(df.iloc[offset:offset+last].copy())
+        batch = pd.DataFrame(dataframe.iloc[offset:offset+last].copy())
         #print(batch.head())
         batch['text'] = batch['text'].apply(parse_text)
         #print(batch.head())
@@ -38,35 +38,36 @@ def main():
         #print(normalized.head())
         batch = batch.join(normalized, how='left', validate='1:1')
         #print(batch.head())
-        batch.to_feather(DIR + f'arxiv_{i}_cols.feather')
-        del batch
-    
-    del df
+        batch.to_feather(dir + f'arxiv_{i}_cols.feather')
 
-    #df = [0]*10
-    
-    #for i in range(10):
-    #    df[i]['text'] = df[i]['text'].apply(parse_text)
-    #    df[i] = 0
-
-    # df['text'] = df['text'].apply(parse_text)
-    # df = df.join(pd.json_normalize(df['text']), how='left', validate='1:1').drop(columns=['text'])
-
-    df = [0]*10
-    for i in range(10):
-        df[i] = pd.read_feather(DIR + f'arxiv_{i}_cols.feather')
+def read_batches(num_batches, dir=DIR):
+    df = [0]*num_batches
+    for i in range(num_batches):
+        df[i] = pd.read_feather(dir + f'arxiv_{i}_cols.feather')
     
     print("Loaded partials")
     df = pd.concat(df)
     print(len(df))
     print(df.head())
-    df.to_parquet(DIR + 'arxiv_all.parquet')
+    return df
+
+def to_parquet(df, dir=DIR):
+    df.to_parquet(dir + 'arxiv_all.parquet')
     df['index'] = df.index
     df = df[['index', 'id']]
-    df.to_parquet(DIR + 'arxiv_all_small.parquet')
+    df.to_parquet(dir + 'arxiv_all_small.parquet')
 
     df = pd.read_parquet(DIR + 'arxiv_all_small.parquet')
     print(df.head())
+
+def main():
+    df = load_json(JSON_FILE)
+
+    write_batches(df, 10, DIR)
+
+    df = read_batches(10, DIR)
+
+    to_parquet(df, DIR)
 
 if __name__=="__main__":
     main()
